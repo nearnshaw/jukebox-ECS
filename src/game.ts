764c32c259d72c6d63f3a24a58e8454f042d7868
Nico E <earnshaw.nico@gmail.com>
2018-11-12 12:35:23 -0300

    euler angles and system interface

--- src/game.ts
+++ src/game.ts
@@ -33,7 +33,7 @@ const buttons = engine.getComponentGroup(Transform, ButtonState)
 ///////////////////////////
 // Systems
 
-export class PushButton {
+export class PushButton implements ISystem {
   update(dt: number) {
     for (let button of buttons.entities) {
       let transform = button.get(Transform)
@@ -62,7 +62,7 @@ const jukebox = new Entity()
 jukebox.set(new GLTFShape("models/Jukebox.gltf"))
 jukebox.set(new Transform())
 jukebox.get(Transform).position.set(5, 0, 9.5)
-jukebox.get(Transform).rotation.set(0, 0, 0)
+jukebox.get(Transform).rotation.setEuler(0, 0, 0)
 jukebox.get(Transform).scale.setAll(0.6)
 engine.addEntity(jukebox)
 
@@ -81,7 +81,7 @@ for (let i = 0; i < songs.length; i ++){
   buttonArray[i] = new Entity()
   buttonArray[i].set(new Transform())
   buttonArray[i].get(Transform).position.set(0, 0, 0)
-  buttonArray[i].get(Transform).rotation.set(90, 0, 0)
+  buttonArray[i].get(Transform).rotation.setEuler(90, 0, 0)
   buttonArray[i].get(Transform).scale.set(0.05, 0.2, 0.05)
   buttonArray[i].parent = buttonWrapper
   buttonArray[i].set(new CylinderShape())
@@ -128,13 +128,13 @@ function pressButton(i:number){
     }
 }
 
-// executeTask(async () => {
-//   try {
-//     await playSound("a.mp3", {
-//       loop: false,
-//       volume: 100,
-//     })
-//   } catch {
-//     log('failed to play sound')
-//   }
-// })
+executeTask(async () => {
+  try {
+    await playSound("a.mp3", {
+      loop: false,
+      volume: 100,
+    })
+  } catch {
+    log('failed to play sound')
+  }
+})
