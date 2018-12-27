b788672a01788ee3445a4040d7ab4adf4c1de3a4
Nico E <earnshaw.nico@gmail.com>
2018-12-27 11:10:32 -0300

    nicer syntax

--- src/game.ts
+++ src/game.ts
@@ -1,3 +1,4 @@
+// import sound controller
 import { playSound } from '@decentraland/SoundController'
 
 // Define song list
@@ -34,7 +35,7 @@ const buttons = engine.getComponentGroup(Transform, ButtonState)
 // Systems
 
 export class PushButton implements ISystem {
-  update(dt: number) {
+  update() {
     for (let button of buttons.entities) {
       let transform = button.get(Transform)
       let state = button.get(ButtonState)
@@ -60,10 +61,11 @@ engine.addSystem(new PushButton)
 // Jukebox
 const jukebox = new Entity()
 jukebox.set(new GLTFShape("models/Jukebox.gltf"))
-jukebox.set(new Transform())
-jukebox.get(Transform).position.set(5, 0, 9.5)
-jukebox.get(Transform).rotation.setEuler(0, 0, 0)
-jukebox.get(Transform).scale.setAll(0.6)
+jukebox.set(new Transform({
+  position: new Vector3(5, 0, 9.5),
+  rotation: Quaternion.Euler(0, 0 ,0),
+  scale: new Vector3(0.6, 0.6, 0.6)
+}))
 engine.addEntity(jukebox)
 
 
@@ -79,15 +81,18 @@ for (let i = 0; i < songs.length; i ++){
   let posX = i % 2 == 0 ? -.4 : .1;
   let posY = Math.floor(i / 2) == 0 ? 1.9 : 1.77;
 
+  // groups the button itself and label
   const buttonWrapper = new Entity()
-  buttonWrapper.set(new Transform())
-  buttonWrapper.get(Transform).position.set(posX, posY, -0.7)
+  buttonWrapper.set(new Transform({
+    position: new Vector3(posX, posY, -0.7)
+  }))
   buttonWrapper.setParent(jukebox)
   engine.addEntity(buttonWrapper)
 
   const buttonLabel = new Entity()
-  buttonLabel.set(new Transform())
-  buttonLabel.get(Transform).position.set(0.6, 0, -0.1)
+  buttonLabel.set(new Transform({
+    position: new Vector3(0.6, 0, -0.1)
+  }))
   const text = new TextShape(songs[i].name)
   text.fontSize = 35
   text.fontFamily = "serif"
@@ -98,15 +103,16 @@ for (let i = 0; i < songs.length; i ++){
   engine.addEntity(buttonLabel)
 
   buttonArray[i] = new Entity()
-  buttonArray[i].set(new Transform())
-  buttonArray[i].get(Transform).position.set(0, 0, 0)
-  buttonArray[i].get(Transform).rotation.setEuler(90, 0, 0)
-  buttonArray[i].get(Transform).scale.set(0.05, 0.2, 0.05)
+  buttonArray[i].set(new Transform({
+    position: new Vector3(0, 0, 0),
+    rotation: Quaternion.Euler(90, 0, 0),
+    scale: new Vector3(0.05, 0.2, 0.05)
+  }))
   buttonArray[i].set(buttonMaterial)
   buttonArray[i].setParent(buttonWrapper)
   buttonArray[i].set(new CylinderShape()) 
   buttonArray[i].set(new ButtonState(0, 0.1))
-  buttonArray[i].set(new OnClick( _ => {
+  buttonArray[i].set(new OnClick( e => {
     pressButton(i)
   }))
 
